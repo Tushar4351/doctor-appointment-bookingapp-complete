1c5437280fb2eeafb21604d52c01be5178a24eff
Tushar Bhowal <tusharbhowal3211@gmail.com>
2024-04-02 00:05:41 +0530

    catagory and doctor list complete

--- doctor-appointment-bookingapp/app/_components/CategorySearch.jsx
+++ doctor-appointment-bookingapp/app/_components/CategorySearch.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
 const CategorySearch = () => {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((res) => {
+      // console.log(res.data.data);
+      setCategoryList(res.data.data);
+    });
+  };
+
   return (
     <div className="mb-10 items-center px-5 flex flex-col gap-2">
       <h2
@@ -22,6 +39,33 @@ const CategorySearch = () => {
           <Search className="h-4 w-4 mr-2" />
           Search
         </Button>
+      </div>
+
+      {/* Display List of Category  */}
+      <div className="grid grid-cols-3 mt-5 md:grid-cols-4 lg:grid-cols-6 ">
+        {categoryList.map(
+          (item, index) =>
+            index < 6 && (
+              <div
+                // href={"/search/" + item.attributes.Name}
+                key={index}
+                className="flex 
+          flex-col text-center items-center
+          p-5 bg-blue-50 m-2 rounded-lg cursor-pointer
+          gap-2 hover:scale-110 transition-all ease-in-out"
+              >
+                <Image
+                  src={item.attributes?.Icon?.data.attributes?.url} //The question marks (?.) are used for optional chaining in JavaScript. They allow accessing nested properties safely, avoiding errors if any intermediate property is null or undefined.
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <label className="text-blue-600 text-sm">
+                  {item?.attributes?.Name}
+                </label>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
--- doctor-appointment-bookingapp/app/page.js
+++ doctor-appointment-bookingapp/app/page.js
@@ -1,11 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import CategorySearch from "./_components/CategorySearch";
+import DoctorList from "./_components/DoctorList";
 import Hero from "./_components/Hero";
+import GlobalApi from "./_utils/GlobalApi";
 
 export default function Home() {
+  const [doctorList, setDoctorList] = useState([]);
+
+  useEffect(() => {
+    getDoctorList();
+  }, []);
+  const getDoctorList = () => {
+    GlobalApi.getDoctorList().then((res) => {
+      console.log(res.data.data);
+      setDoctorList(res.data.data);
+    });
+  };
+
   return (
     <div>
       <Hero />
       <CategorySearch />
+      <DoctorList doctorList={doctorList} />
     </div>
   );
 }
